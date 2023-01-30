133c77644086e27a2b99a765ac8b80dcb3e789a6
Kamil Seweryn <kamil_seweryn@o2.pl>
2023-01-30 22:37:51 +0100

    init commit

---
+++ src/main/java/com/workvaluation/workvaluation/WorkValuationApplication.java
@@ -0,0 +1,13 @@
+package com.workvaluation.workvaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkValuationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkValuationApplication.class, args);
+	}
+
+}

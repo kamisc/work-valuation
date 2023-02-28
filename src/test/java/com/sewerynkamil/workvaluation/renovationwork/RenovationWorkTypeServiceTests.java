25b3ee1f89157f6b55bde19fbac5b57ad85c2261
Kamil Seweryn <kamil_seweryn@o2.pl>
2023-02-28 22:29:01 +0100

    add tests methods for tdd approach

---
+++ src/test/java/com/sewerynkamil/workvaluation/renovationwork/RenovationWorkTypeServiceTests.java
@@ -0,0 +1,56 @@
+package com.sewerynkamil.workvaluation.renovationwork;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RenovationWorkTypeServiceTests {
+
+    @BeforeAll
+    void setUpData() {
+
+    }
+
+    @Test
+    void testGetAllRenovationWorkTypes() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void testGetRenovationWorkTypeById() {
+
+    }
+
+    @Test
+    void testGetRenovationWorkTypeByIdNotFound() {
+
+    }
+
+    @Test
+    void testGetAddRenovationWorkType() {
+
+    }
+
+    @Test
+    void testUpdateRenovationWorkType() {
+
+    }
+
+    @Test
+    void testPartialUpdateRenovationWork() {
+
+    }
+
+    @Test
+    void testDeleteRenovationWork() {
+
+    }
+}
